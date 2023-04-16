a515ee924953d20baf777d77838a9ff69027f578
Samta <108336676+Smta028@users.noreply.github.com>
2023-04-16 12:57:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function insert_Row() {
     //Write your code here
+	var count=1;
 	 var table = document.getElementById("sampleTable");
   var row = table.insertRow(0);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
-  cell1.innerHTML = "NEW CELL1";
-  cell2.innerHTML = "NEW CELL2";
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
 }
